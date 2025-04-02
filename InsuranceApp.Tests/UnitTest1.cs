322d9a04f66eaf502406c08e146b57662d218088
GarryLed <S00256356@atu.ie>
2025-04-02 14:44:56 +0100

    Add project files.

---
+++ InsuranceApp.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace InsuranceApp.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ InsuranceApp/IDiscountService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceApp
+{
+    /// <summary>
+    /// Interface for Discount Service (decouples the Discount Service from the Insurance Service)
+    /// </summary>
+    public interface IDiscountService
+    {
+        double GetDiscount();
+    }
+}
